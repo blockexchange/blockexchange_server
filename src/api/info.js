a5f2230ee82e095390f0395143f5f1ec687e1f46
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-31 17:31:29 +0200

    major/minor version

--- src/api/info.js
+++ src/api/info.js
@@ -4,7 +4,8 @@ app.get('/api/info', function(req, res){
   console.log("GET /api/info");
 
 	res.json({
-		api_version: "1.0.0",
+		api_version_major: 1,
+		api_version_minor: 1,
 		name: process.env.BLOCKEXCHANGE_NAME || "unknown",
 		owner: process.env.BLOCKEXCHANGE_OWNER || "unknown",
 	});
