c5c2f2ee785557efe056bf52369d8cc8604ccad3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-02 16:22:27 +0200

    extended integration test

--- integration-test.sh
+++ integration-test.sh
@@ -30,6 +30,28 @@ trap cleanup EXIT
 
 bash -c 'while !</dev/tcp/localhost/8080; do sleep 1; done;'
 
-# TODO download mod and start minetest
+git clone https://github.com/blockexchange/blockexchange.git
+
+CFG=/tmp/minetest.conf
+MTDIR=/tmp/mt
+WORLDDIR=${MTDIR}/worlds/world
+
+cat <<EOF > ${CFG}
+ blockexchange.url = http://localhost:8080
+ enable_blockexchange_integration_test = true
+ secure.http_mods = blockexchange
+EOF
+
+mkdir -p ${WORLDDIR}
+chmod 777 ${MTDIR} -R
+docker run --rm -i \
+	-v ${CFG}:/etc/minetest/minetest.conf:ro \
+	-v ${MTDIR}:/var/lib/minetest/.minetest \
+	-v $(pwd)/blockexchange:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange \
+  --network host \
+	registry.gitlab.com/minetest/minetest/server:5.1.0
+
+test -f ${WORLDDIR}/integration_test.json && exit 0 || exit 1
+
 
 echo "Test complete!"
