ee03cac7b6f96c165caa3edb7834bf93653b6c71
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-07 11:43:22 +0200

    fix file path in test-mod

--- integration-test.sh
+++ integration-test.sh
@@ -39,7 +39,6 @@ WORLDDIR=${MTDIR}/worlds/world
 
 cat <<EOF > ${CFG}
  blockexchange.url = http://localhost:8080
- enable_blockexchange_integration_test = true
  secure.http_mods = blockexchange
 EOF
 
@@ -48,7 +47,8 @@ chmod 777 ${MTDIR} -R
 docker run --rm -i \
 	-v ${CFG}:/etc/minetest/minetest.conf:ro \
 	-v ${MTDIR}:/var/lib/minetest/.minetest \
-	-v $(pwd)/blockexchange:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange \
+  -v $(pwd)/blockexchange:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange \
+  -v $(pwd)/blockexchange/test/test_mod:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange_test \
   --network host \
 	registry.gitlab.com/minetest/minetest/server:5.1.0
 
