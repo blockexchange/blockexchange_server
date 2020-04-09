86a080653e0b87d3c5f4b107727b24260e0db890
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-09 08:12:46 +0200

    add prometheus monitoring of http endpoints
    fixes #12

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const promMid = require('express-prometheus-middleware');
 
 const app = express();
 app.use(express.static('public'));
 app.disable('etag');
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  requestDurationBuckets: [0.01, 0.1, 0.25, 0.5, 1.0],
+}));
 
 module.exports = app;
