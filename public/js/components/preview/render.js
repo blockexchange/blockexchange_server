3752e2862919947c95b59f199b68a8c6d6574c22
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-14 21:44:08 +0200

    rename variables

--- public/js/components/preview/render.js
+++ public/js/components/preview/render.js
@@ -9,35 +9,35 @@ export default function(scene, schema, posx, posy, posz){
 	const block_z = Math.floor(posz * schema.part_length);
 
   return m.request(`api/schemapart/${schema.id}/${block_x}/${block_y}/${block_z}`)
-  .then(function(mapblock){
-    if (!mapblock)
+  .then(function(schemapart){
+    if (!schemapart)
       return;
 
-		if (Object.keys(mapblock.data.node_mapping).length == 1 && mapblock.data.node_mapping.air) {
+		if (Object.keys(schemapart.data.node_mapping).length == 1 && schemapart.data.node_mapping.air) {
       return;
     }
 
 		// create reverse mapping: nodeId -> nodeName
 		const node_mapping_rev = {};
-		Object.keys(mapblock.data.node_mapping).forEach(nodeName => {
-			const nodeId = mapblock.data.node_mapping[nodeName];
+		Object.keys(schemapart.data.node_mapping).forEach(nodeName => {
+			const nodeId = schemapart.data.node_mapping[nodeName];
 			node_mapping_rev[nodeId] = nodeName;
 		});
-		mapblock.data.node_mapping_rev = node_mapping_rev;
+		schemapart.data.node_mapping_rev = node_mapping_rev;
 
     var nodenameGeometriesMap = {}; // nodeName => [matrix, matrix, ...]
 
-		for (var x=0; x<16; x++){
-			for (var y=0; y<16; y++){
-	      for (var z=0; z<16; z++){
-          if (isNodeHidden(mapblock, x,y,z)){
+		for (var x=0; x<schemapart.data.size.x; x++){
+			for (var y=0; y<schemapart.data.size.y; y++){
+	      for (var z=0; z<schemapart.data.size.z; z++){
+          if (isNodeHidden(schemapart, x,y,z)){
             //skip hidden node
             continue;
           }
 
 					var i = getNodePos(x,y,z);
-          var contentId = mapblock.data.node_ids[i];
-          var nodeName = mapblock.data.node_mapping_rev[contentId];
+          var contentId = schemapart.data.node_ids[i];
+          var nodeName = schemapart.data.node_mapping_rev[contentId];
 
           var matrix = new THREE.Matrix4()
             .makeTranslation(
