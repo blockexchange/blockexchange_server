727992d6c528066d348a0bab4e3be8f36768f241
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-16 11:08:13 +0200

    add login/register stub

--- public/js/routes.js
+++ public/js/routes.js
@@ -2,9 +2,13 @@ import Start from './views/Start.js';
 import Search from './views/Search.js';
 import UserSchemaList from './views/UserSchemaList.js';
 import SchemaDetail from './views/SchemaDetail.js';
+import Login from './views/Login.js';
+import Register from './views/Register.js';
 
 export default {
   "/": Start,
+  "/login": Login,
+  "/register": Register,
   "/search": Search,
   "/schema/:username": UserSchemaList,
   "/schema/:username/:schemaname": SchemaDetail
