0037312f3aeb315ce83c03846300a219ab08da86
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-17 09:01:24 +0200

    fix tracker error

--- public/js/util/matomo.js
+++ public/js/util/matomo.js
@@ -1,10 +1,10 @@
 
 export function setup_tracker(url, id){
   window._paq = [
-    'trackPageView',
-    'enableLinkTracking',
-    'setTrackerUrl', url + "matomo.php",
-    'setSiteId', id
+    ['trackPageView'],
+    ['enableLinkTracking'],
+    ['setTrackerUrl', url + "matomo.php"],
+    ['setSiteId', id]
   ];
 
   const tag = document.createElement('script');
