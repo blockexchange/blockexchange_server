f951769df410be4663ccafdae0d86df2846c32e7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-19 18:23:02 +0200

    red badge if ignore mod found

--- public/js/components/ModList.js
+++ public/js/components/ModList.js
@@ -8,7 +8,7 @@ export default {
   view: function(vnode) {
     const schema = vnode.attrs.schema;
     return m("div", Object.keys(schema.mods).map(mod_name => {
-    	return badge("secondary", mod_name);
+    	return badge(mod_name == "ignore" ? "danger" : "secondary", mod_name);
     }));
   }
 };
