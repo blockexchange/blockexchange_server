0507f0df46e48aaf2b3eb9fe15f463b2b3e85429
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-20 11:20:52 +0200

    GET star path

--- src/api/schema_star.js
+++ src/api/schema_star.js
@@ -6,7 +6,16 @@ const userschemastar_dao = require("../dao/userschemastar");
 const tokenmiddleware = require("../middleware/token");
 const tokencheck = tokenmiddleware();
 
-app.post('/api/schema/:id/star', tokencheck, jsonParser, function(req, res){
+app.get('/api/schema/:id/star', jsonParser, function(req, res){
+  console.log("GET /api/schema/:id/star", req.params.id);
+
+	userschemastar_dao.find_by_schema_id(req.params.id)
+	.then(result => res.json(result))
+	.catch(() => res.status(500).end());
+
+});
+
+app.put('/api/schema/:id/star', tokencheck, jsonParser, function(req, res){
   console.log("POST /api/schema/:id/star", req.params.id, req.claims.user_id);
 
 	userschemastar_dao.create(req.claims.user_id, req.params.id)
