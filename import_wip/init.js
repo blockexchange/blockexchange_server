0614fe57b407a41570dc57b13d043ff10f8950a1
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-20 15:53:46 +0200

    debug we import

--- import_wip/init.js
+++ import_wip/init.js
@@ -1,7 +1,7 @@
 const luaparse = require("./luaparse");
 const fs = require("fs");
 
-const schema = fs.readFileSync('./blockexchange.we');
+const schema = fs.readFileSync('./prometheus.we');
 
 const cleanlua = schema.toString().substring(2);
 const ast = luaparse.parse(cleanlua);
@@ -15,6 +15,24 @@ function unbox(field){
 		return +field.raw;
 	if (field.type == "StringLiteral")
 		return field.raw.substring(1, field.raw.length-1);
+	if (field.type == "TableConstructorExpression"){
+		const entry = {};
+		field.fields.forEach(function(f){
+			if (!f.key || !f.value){
+				throw new Error(JSON.stringify(f));
+			}
+			if (!f.key && f.value == "StringLiteral"){
+				entry = [];
+				entry.push(unbox(f.value));
+				return;
+			}
+
+			const key = unbox(f.key);
+			const value = unbox(f.value);
+			entry[key] = value;
+		});
+		return entry;
+	}
 }
 
 ast.body[0].arguments[0].fields.forEach(function(o){
@@ -25,10 +43,14 @@ ast.body[0].arguments[0].fields.forEach(function(o){
 	o.value.fields
 	.filter(f => f.type == "TableKey")
 	.forEach(function(f){
+		//console.log(f);
 		const key = unbox(f.key);
 		const value = unbox(f.value);
-		entry[key] = value;
+
+		if (key){
+			entry[key] = value;
+		}
 	});
 
-	console.log(entry);
+	console.log(JSON.stringify(entry));
 });
