5ebaff2ed4a8dfa0eb6cfda2d9e82054761130b1
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-20 11:49:00 +0200

    add more license names

--- public/js/components/LicenseBadge.js
+++ public/js/components/LicenseBadge.js
@@ -2,8 +2,14 @@
 export default {
   view: function(vnode){
     switch (vnode.attrs.license) {
-  		case "CC0":
+      case "CC0":
   			return m("img", {src:"pics/license_cc0.png"});
+      case "CC-BY":
+  			return m("img", {src:"pics/license_cc-by.png"});
+      case "CC-BY-SA":
+  			return m("img", {src:"pics/license_cc-by-sa.png"});
+      case "CC-BY-NC":
+  			return m("img", {src:"pics/license_cc-by-nc.png"});
   		default:
   			return m("span", {class: "badge badge-primary"}, vnode.attrs.license);
   	}
