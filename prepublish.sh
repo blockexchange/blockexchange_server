826592c40cd6b6b890bc0bcb766c61c92a2cd81c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-07-08 22:01:32 +0200

    use libraries from npm

---
+++ prepublish.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+cp node_modules/htm/dist/htm.js public/js/lib/
+cp node_modules/luaparse/luaparse.js public/js/lib/
+cp node_modules/mithril/mithril.min.js public/js/lib/
+cp node_modules/moment/min/moment.min.js public/js/lib/
+cp node_modules/three/build/three.min.js public/js/lib/
