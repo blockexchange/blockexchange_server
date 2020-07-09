f2a11839771cbd0cf88048b3eacab64fcbb461a8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-07-09 13:17:13 +0200

    add missing class

--- public/js/components/schemadetail/EditButton.js
+++ public/js/components/schemadetail/EditButton.js
@@ -9,7 +9,7 @@ export default {
 
     if (claims && claims.user_id == schema.user_id && claims.permissions.schema.update){
       return html`
-        <button class="secondary" href="#!/schema/${schema.user.name}/${schema.name}/edit">
+        <button class="btn btn-secondary" href="#!/schema/${schema.user.name}/${schema.name}/edit">
           <i class="fa fa-edit"/> Edit
         </button>
       `;
