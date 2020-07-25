7fd5f6994ff8b552be4bc2f4426a440950919e8a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-07-25 10:10:30 +0200

    format discord hook

--- src/feed/discord.js
+++ src/feed/discord.js
@@ -1,4 +1,5 @@
 const events = require("../events");
+const user_dao = require("../dao/user");
 const axios = require('axios');
 
 module.exports = function(){
@@ -9,19 +10,18 @@ module.exports = function(){
   if (schema_feed_url){
     events.on("new-schema", function(schema){
       //https://birdie0.github.io/discord-webhooks-guide/examples/spotify.html
-      const data = {
-        content: `Schema created: **${schema.name}**
-        License: **${schema.license}**
-        Size: ${schema.size_x}/${schema.size_y}/${schema.size_z} Blocks / ${schema.total_size} bytes
-        Description:
-        \`\`\`
-        ${schema.description}
-        \`\`\`
-        `
-      };
+      user_dao.get_by_id(schema.user_id)
+      .then(function(user){
+        const data = {
+          content: `Schema created: **${schema.name}** by **${user.name}**\n` +
+            `License: **${schema.license}**\n` +
+            `Size: ${schema.size_x}/${schema.size_y}/${schema.size_z} Blocks / ${schema.total_size} bytes\n` +
+            `Description:\n\`\`\`\n${schema.description}\n\`\`\`\n`
+        };
 
-      axios.post(schema_feed_url, data);
-      //TODO: check status
+        axios.post(schema_feed_url, data);
+        //TODO: check status
+      });
     });
   }
 };
