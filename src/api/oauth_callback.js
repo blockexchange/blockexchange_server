1d2e52bd4d2eb24a91a8ae870d5316d77fb8e4f6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-11-30 07:32:08 +0100

    github login

--- src/api/oauth_callback.js
+++ src/api/oauth_callback.js
@@ -10,9 +10,34 @@ app.get('/api/oauth_callback', function(req, res){
     client_secret: "xxx",
     code: req.query.code
   };
-  axios.post("https://github.com/login/oauth/access_token", data)
-  .then(r => console.log(r));
 
+  const options = {
+    headers: {
+      "Accept": "application/json"
+    }
+  };
 
-	res.end();
+  axios.post("https://github.com/login/oauth/access_token", data, options)
+  .then(r => {
+    if (!r.data.access_token){
+      console.log(r);
+      throw new Error("No access_token received!");
+    }
+    return axios.get("https://api.github.com/user", {
+      headers: {
+        "Authorization": "Bearer " + r.data.access_token
+      }
+    });
+  })
+  .then(r => {
+    const user_info = r.data;
+    console.log(user_info);
+    // user_info.login / avatar_url / name / email
+    //TODO: build jwt
+    res.redirect("http://127.0.0.1:8080/#/oauth?jwt=blah");
+  })
+  .catch(e => {
+    console.log(e);
+    res.send(e.messages);
+  });
 });
