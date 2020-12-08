e4b0a6d7d0137dc217038f3f3f2245c775cfa977
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-08 21:37:31 +0100

    selenium wip

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -1,14 +1,13 @@
-const {Builder} = require('selenium-webdriver');
+const {Builder, By, until} = require('selenium-webdriver');
 
 (async function example() {
   let driver = await new Builder()
-    .usingServer('http://blockexchange_selenium:4444/wd/hub')
-    .forBrowser('chrome')
+    .usingServer('http://selenium:4444/wd/hub')
+    .forBrowser('firefox')
     .build();
   try {
-    await driver.get('http://blockexchange_server:8080/');
-    //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get('http://blockexchange:8080/#/');
+    await driver.wait(until.elementLocated(By.className('img-fluid')), 10000);
   } finally {
     await driver.quit();
     console.log("e2e tests done!");
