4b86692ae236045096e14cfe5ba594813a50bd0d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-08 20:28:42 +0100

    token parsing in oauth frontend

--- public/js/components/pages/OauthPage.js
+++ public/js/components/pages/OauthPage.js
@@ -1,6 +1,12 @@
+import loginStore from '../../store/login.js';
+
 export default {
 	beforeRouteEnter: function(to, from, next){
-		console.log(to.params.token);
+		const payload = JSON.parse(atob(to.params.token.split(".")[1]));
+		loginStore.token = to.params.token;
+		loginStore.loggedIn = true;
+		loginStore.username = payload.username;
+
 		next({ path: "/login" });
 	},
 	template: `<div></div>`
