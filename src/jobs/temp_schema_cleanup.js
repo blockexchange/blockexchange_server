409c80886d02c2a267b5453149c4762bf8602af3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-10 13:05:02 +0100

    fix wrong week constant

--- src/jobs/temp_schema_cleanup.js
+++ src/jobs/temp_schema_cleanup.js
@@ -1,8 +1,8 @@
 const schema_dao = require("../dao/schema");
+const millis_in_one_week = 7*24*60*60*1000;
 
 function job(){
   const now = Date.now();
-  const millis_in_one_week = 7*24*60*1000;
   schema_dao.delete_old_temp_schemas(now - millis_in_one_week);
 }
 
