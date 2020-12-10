6922a60f3c0ac62242391b2b21d251ceedaeb8bc
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-10 11:15:25 +0100

    colormapping / render skeleton

--- test/render.js
+++ test/render.js
@@ -4,6 +4,29 @@ const { createCanvas } = require('canvas');
 
 describe('renderer', function() {
 	it('renders an image', function() {
+		const mapblock = JSON.parse(fs.readFileSync("test/schemapart_2_(0,0,0).json"));
+		const colormapping = JSON.parse(fs.readFileSync("test/colormapping.json"));
+
+		function get_point(x,y,z){
+			const index = x + (y*16) + (z*256);
+			return mapblock.node_ids[index];
+		}
+
+		// reverse index
+		const nodeid_mapping = [];
+		Object.keys(mapblock.node_mapping).forEach(function(nodename){
+			nodeid_mapping[mapblock.node_mapping[nodename]] = nodename;
+		});
+
+		function get_color(nodeid){
+			const nodename = nodeid_mapping[nodeid];
+			return colormapping[nodename];
+		}
+
+		console.log(get_point(0,0,0), get_color(get_point(0,0,0)));
+		console.log(get_point(15,15,15), get_color(get_point(15,15,15)));
+
+
 		const width = 1200;
 		const height = 600;
 
