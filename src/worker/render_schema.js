5bfb6b5a669b44bd08b7b98188058bc08feae9b0
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-15 08:30:35 +0100

    cleanups

--- src/worker/render_schema.js
+++ src/worker/render_schema.js
@@ -1,13 +1,10 @@
 const SchemaRenderer = require("../render/SchemaRenderer");
 const { create } = require("../dao/schema_screenshot");
 
-console.log(process.argv);
-
 const schema_id = +process.argv[2];
 
 SchemaRenderer.render(schema_id)
 .then(png => {
 	// save png
-	console.log("png", schema_id, png.length);
 	create(schema_id, "Preview", "image/png", png);
 });
