bc0c86cc81f3057d6a31907bb7b1d72fcd42c39b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-12-26 15:41:38 +0100

    /bx_token creation

--- public/js/components/pages/ProfilePage.js
+++ public/js/components/pages/ProfilePage.js
@@ -1,12 +1,16 @@
 import UserProfile from '../user/UserProfile.js';
+import CreateToken from '../user/CreateToken.js';
 
 export default {
 	components: {
-		"user-profile": UserProfile
+		"user-profile": UserProfile,
+		"create-token": CreateToken
 	},
 	template: /*html*/`
 		<div>
 			<user-profile/>
+			<br>
+			<create-token/>
 		</div>
 	`
 };
