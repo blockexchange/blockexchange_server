f145a932e282d886b5c0c9709023436625ed4d57
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-06 19:41:13 +0100

    re-enable analytics

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,7 @@ import { get_info } from './api/info.js';
 
 import './util/prettysize-filter.js';
 
-function start(){
+function start(info){
 	// try to restore state
 	loginService.restoreState();
 
@@ -15,6 +15,15 @@ function start(){
 	const router = new VueRouter({
 	  routes: routes
 	});
+
+	if (info.matomo.url){
+		// Enable analytics
+		Vue.use(VueMatomo, {
+			host: info.matomo.url,
+			siteId: info.matomo.id,
+			router: router
+		});
+	}
 
 	const i18n = new VueI18n({
 		fallbackLocale: 'en',
@@ -33,5 +42,5 @@ get_info().then(info => {
 	Object.keys(info).forEach(key => {
 		infoStore[key] = info[key];
 	});
-	start();
+	start(info);
 });
