4845656937ea261640053dd47b49c8e7a6fef8fe
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-19 09:13:39 +0100

    default token has half a year expiration

--- src/util/setupuser.js
+++ src/util/setupuser.js
@@ -2,7 +2,7 @@ const { create, generateToken } = require("../dao/access_token");
 
 // Sets up the existing user with some defaults (access_token, etc)
 module.exports = async function(user){
-	// generate a "default" access_token
-	await create(user.id, Date.now(), Date.now() + (1000*3600*24*7), "default", generateToken());
+	// generate a "default" access_token with half a year expiration
+	await create(user.id, Date.now(), Date.now() + (1000*3600*24*31*6), "default", generateToken());
 	return user;
 };
