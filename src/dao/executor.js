962275dbf57a5897c3197051ae60d3d347321962
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-25 11:42:56 +0100

    use pg pool directly for queries

--- src/dao/executor.js
+++ src/dao/executor.js
@@ -3,10 +3,8 @@ const pool = require("../pool");
 module.exports = async function(query, values, options){
 	const single_row = options && options.single_row;
 
-	let client;
 	try {
-		client = await pool.connect();
-		const sql_res = await client.query(query, values);
+		const sql_res = await pool.query(query, values);
 		if (!sql_res.rows || sql_res.rows.length == 0){
 			// no result
 			return single_row ? null : [];
@@ -16,7 +14,5 @@ module.exports = async function(query, values, options){
 	} catch (e) {
 		console.error(e.stack);
 		throw(e);
-	} finally {
-		client.release();
 	}
 };
