423b45c2045e416e98a1c3efd660ac134f8cba53
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-03-08 21:00:12 +0100

    cache screenshot metadata

--- public/js/api/screenshot.js
+++ public/js/api/screenshot.js
@@ -1,2 +1,14 @@
-export const get_by_schemaid = schema_id => fetch(`api/schema/${schema_id}/screenshot`)
-	.then(r => r.json());
+
+const cache = {};
+
+export const get_by_schemaid = schema_id => {
+	if (cache[schema_id]){
+		return cache[schema_id];
+	}
+	fetch(`api/schema/${schema_id}/screenshot`)
+	.then(r => {
+		const json = r.json();
+		cache[schema_id] = json;
+		return json;
+	});
+};
