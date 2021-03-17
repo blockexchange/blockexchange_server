71637f266a62c2072169e49f5a97986b9334e76d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-03-17 14:10:32 +0100

    parser stub

---
+++ render/parser.go
@@ -0,0 +1,105 @@
+package render
+
+import (
+	"blockexchange/types"
+	"bytes"
+	"compress/zlib"
+	"encoding/json"
+)
+
+const offset = 32768
+
+type SchemaPartSize struct {
+	X int
+	Y int
+	Z int
+}
+
+type SchemaPartMetadata struct {
+	NodeMapping map[string]int
+	Size        SchemaPartSize
+}
+
+func getInt(o interface{}) int {
+	v, _ := o.(float64)
+	return int(v)
+}
+
+func (s *SchemaPartMetadata) UnmarshalJSON(data []byte) error {
+	m := make(map[string]json.RawMessage)
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return err
+	}
+
+	nm := make(map[string]float64)
+	err = json.Unmarshal(m["node_mapping"], &nm)
+	if err != nil {
+		return err
+	}
+
+	s.NodeMapping = make(map[string]int)
+	for k, v := range nm {
+		s.NodeMapping[k] = int(v)
+	}
+
+	s.Size = SchemaPartSize{}
+	nm = make(map[string]float64)
+	err = json.Unmarshal(m["size"], &nm)
+	if err != nil {
+		return err
+	}
+
+	s.Size.X = getInt(nm["x"])
+	s.Size.Y = getInt(nm["y"])
+	s.Size.Z = getInt(nm["z"])
+
+	return nil
+}
+
+type ParsedSchemaPart struct {
+	NodeIDS []int16
+	Param1  []byte
+	Param2  []byte
+	Meta    *SchemaPartMetadata
+}
+
+func ParseSchemaPart(part *types.SchemaPart) (*ParsedSchemaPart, error) {
+	r, err := zlib.NewReader(bytes.NewReader(part.MetaData))
+	if err != nil {
+		return nil, err
+	}
+
+	md := SchemaPartMetadata{}
+	err = json.NewDecoder(r).Decode(&md)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err = zlib.NewReader(bytes.NewReader(part.Data))
+	if err != nil {
+		return nil, err
+	}
+
+	result := ParsedSchemaPart{
+		Meta: &md,
+	}
+
+	size := md.Size.X * md.Size.Y * md.Size.Z
+
+	// prepare result buffer
+	result.NodeIDS = make([]int16, size)
+	result.Param1 = make([]byte, size)
+	result.Param2 = make([]byte, size)
+
+	data := make([]byte, size*4)
+	r.Read(data)
+
+	for i := 0; i < size; i++ {
+		result.NodeIDS[i] = int16((int(data[i*2]) * 256) + int(data[(i*2)+1]) - offset)
+		result.Param1[i] = data[(size*2)+i]
+		result.Param2[i] = data[(size*3)+i]
+	}
+
+	return &result, nil
+}
---
+++ render/parser_test.go
@@ -0,0 +1,57 @@
+package render
+
+import (
+	"blockexchange/types"
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntConversion(t *testing.T) {
+	offset := 32768
+	high := 0x80
+	low := 0x03
+
+	value := int16((int(high) * 256) + int(low) - offset)
+	assert.Equal(t, int16(3), value)
+}
+
+func TestParseSchemaPart(t *testing.T) {
+	f, err := os.Open("testdata/0_0_0.json")
+	assert.NoError(t, err)
+	assert.NotNil(t, f)
+
+	part := types.SchemaPart{}
+	err = json.NewDecoder(f).Decode(&part)
+	assert.NoError(t, err)
+	assert.NotNil(t, f)
+
+	parsed, err := ParseSchemaPart(&part)
+	assert.NoError(t, err)
+	assert.NotNil(t, parsed)
+	assert.NotNil(t, parsed.Meta)
+	assert.NotNil(t, parsed.Meta.NodeMapping)
+	assert.NotNil(t, parsed.Meta.Size)
+	assert.NotNil(t, parsed.NodeIDS)
+	assert.NotNil(t, parsed.Param1)
+	assert.NotNil(t, parsed.Param2)
+
+	id_node_mapping := make(map[int16]string)
+	for k, v := range parsed.Meta.NodeMapping {
+		id_node_mapping[int16(v)] = k
+	}
+	for i, v := range parsed.NodeIDS {
+		nodename := id_node_mapping[v]
+		if nodename == "" {
+			t.Errorf("node_id not found: %d @ position %d", v, i)
+		}
+	}
+
+	for i, v := range parsed.Param1 {
+		if v > 15 {
+			t.Errorf("param1 too high: %d @ position %d", v, i)
+		}
+	}
+}
