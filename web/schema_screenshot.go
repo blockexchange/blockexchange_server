213c537cf6c27f0c2f4d0010d6cb5a375be8965e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-03-23 07:24:15 +0100

    switch to bilinear image resizing

--- web/schema_screenshot.go
+++ web/schema_screenshot.go
@@ -54,7 +54,7 @@ func (api Api) GetSchemaScreenshotByID(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		newImage := resize.Resize(uint(width), uint(height), img, resize.Lanczos2)
+		newImage := resize.Resize(uint(width), uint(height), img, resize.Bilinear)
 		w.Header().Set("Content-Type", "image/png")
 		err = png.Encode(w, newImage)
 		if err != nil {
