6a500fa17d621d7cd60c716981ddf4339e0be127
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-03-28 19:51:52 +0200

    add web test stub

--- web/serve_test.go
+++ web/serve_test.go
@@ -1,20 +1,29 @@
 package web
 
 import (
+	"encoding/json"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestServe(t *testing.T) {
 	r := mux.NewRouter()
 	ts := httptest.NewServer(r)
+	defer ts.Close()
 
 	api := createTestApi(t)
 	SetupRoutes(r, api)
 
-	//TODO: test stuff
+	res, err := http.Get(ts.URL + "/api/info")
+	assert.NoError(t, err)
 
-	defer ts.Close()
+	info := Info{}
+	err = json.NewDecoder(res.Body).Decode(&info)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, info.VersionMajor)
+	assert.Equal(t, 1, info.VersionMinor)
 }
