e6a7d8f1f1c9c37e2a3dca9d98887205c18262b7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-03-28 13:39:01 +0200

    add list assertion to get tag test

--- web/tag_test.go
+++ web/tag_test.go
@@ -3,6 +3,8 @@ package web
 import (
 	"blockexchange/core"
 	"blockexchange/testutils"
+	"blockexchange/types"
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
@@ -13,10 +15,23 @@ func TestGetTags(t *testing.T) {
 	db_ := testutils.CreateTestDatabase(t)
 	api := NewApi(db_, core.NewNoOpCache())
 
+	err := api.TagRepo.Create(&types.Tag{
+		Name:        "test",
+		Description: "123",
+	})
+	assert.NoError(t, err)
+
 	r := httptest.NewRequest("GET", "http://", nil)
 	w := httptest.NewRecorder()
 
 	api.GetTags(w, r)
 
 	assert.Equal(t, 200, w.Result().StatusCode)
+
+	tags := []types.Tag{}
+	err = json.NewDecoder(w.Body).Decode(&tags)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(tags))
+	assert.Equal(t, "test", tags[0].Name)
+	assert.Equal(t, "123", tags[0].Description)
 }
