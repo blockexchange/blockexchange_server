2680b2fda43d1052278f03df17aa78483922255a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-12-03 16:44:41 +0100

    switch to bootswatch cyborg theme

--- public/embed.go
+++ public/embed.go
@@ -5,7 +5,7 @@ import (
 )
 
 //go:embed js/* pics/* index.html
-//go:embed node_modules/bootstrap/dist/css/bootstrap.min.css
+//go:embed node_modules/bootswatch/dist/cyborg/bootstrap.min.css
 //go:embed node_modules/vue/dist/vue.min.js
 //go:embed node_modules/vue-router/dist/vue-router.min.js
 //go:embed node_modules/vue-i18n/dist/vue-i18n.min.js
