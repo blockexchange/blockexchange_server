d02ea05be4211ed632cffc8c9b895c8185afbb62
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-12-17 06:36:35 +0100

    center image

--- public/js/components/schema/SchemaPreview.js
+++ public/js/components/schema/SchemaPreview.js
@@ -1,7 +1,7 @@
 export default {
 	props: ["screenshots", "schema", "version"],
 	template: /*html*/`
-	<div v-for="screenshot in screenshots">
+	<div class="text-center" v-for="screenshot in screenshots">
 		<img class="img-fluid" :src="'api/schema/' + schema.id + '/screenshot/' + screenshot.id + '?version=' + version"/>
 	</div>
 	`
