3eaf975b2bfdbab8fe9741a4db815cedfa91c4eb
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-06 15:49:35 +0100

    export warning

--- public/js/components/schema/SchemaDownload.js
+++ public/js/components/schema/SchemaDownload.js
@@ -23,6 +23,7 @@ export default {
 				<div class="card-body">
 					<a v-if="schema.total_parts <= 50" class="btn btn-sm btn-primary" :href="'api/export_we/' + schema.id + '/' + schema.name + '.we'">
 						<i class="fa fa-download"/> Export as WE Schema
+						<i class="fa fa-exclamation-triangle" style="color: orange;" title="The WE-export does currently not support metadata"></i>
 					</a>
 					&nbsp;
 					<a class="btn btn-sm btn-primary" :href="'api/export_bx/' + schema.id + '/' + schema.name + '.zip'">
