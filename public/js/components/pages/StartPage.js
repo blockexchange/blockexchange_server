3f03aeba5eda59750fe88f98458e2e12ccf57720
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-06 15:50:48 +0100

    remove unstable warning

--- public/js/components/pages/StartPage.js
+++ public/js/components/pages/StartPage.js
@@ -1,10 +1,6 @@
 import RecentEvents from '../recent/RecentEvents.js';
-import { get_info } from '../../api/info.js';
 
 export default {
-	created: function(){
-		get_info().then(info => this.info = info);
-	},
 	data: function(){
 		return {
 			info: null
@@ -16,18 +12,7 @@ export default {
 	template: /*html*/`
 		<div>
 			<div class="text-center">
-				<div class="alert alert-danger" role="alert">
-					Blockexchange is currently undergoing major refactoring and may be unusable for now
-					<br>
-					Stay tuned for the next official release announcement
-				</div>
-				<hr/>
 				<h4>Exchange your schemas across worlds with ease</h4>
-				<div v-if="info">
-					Version: <span class="badge bg-primary">
-						{{info.api_version_major}}.{{info.api_version_minor}}
-					</span>
-				</div>
 				<hr/>
 
 				<div>
