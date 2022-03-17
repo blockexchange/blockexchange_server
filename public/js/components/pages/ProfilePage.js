01ce210f94e59d74d28ed078f109370afed28d84
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-17 21:02:32 +0100

    show own schemas in profile page

--- public/js/components/pages/ProfilePage.js
+++ public/js/components/pages/ProfilePage.js
@@ -1,16 +1,22 @@
 import UserProfile from '../user/UserProfile.js';
 import AccessToken from '../user/AccessToken.js';
+import UserSchemas from '../search/UserSchemas.js';
+import store from '../../store/login.js';
 
 export default {
+	data: () => store,
 	components: {
 		"user-profile": UserProfile,
-		"access-token": AccessToken
+		"access-token": AccessToken,
+		"user-schemas": UserSchemas
 	},
 	template: /*html*/`
 		<div>
 			<user-profile/>
 			<br>
 			<access-token/>
+			<br>
+			<user-schemas :user_name="claims.username"/>
 		</div>
 	`
 };
