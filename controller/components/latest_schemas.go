2f64893981c7b44721936873eaa9410b8e3beb8d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-08-11 10:18:14 +0200

    cleanup

--- controller/components/latest_schemas.go
+++ controller/components/latest_schemas.go
@@ -2,12 +2,21 @@ package components
 
 import (
 	"blockexchange/db"
+	"blockexchange/types"
 )
 
 type LatestSchemasModel struct {
+	List []*types.SchemaSearchResult
 }
 
 func LatestSchemas(repos *db.Repositories) (*LatestSchemasModel, error) {
 	m := &LatestSchemasModel{}
+	var err error
+
+	m.List, err = repos.SchemaSearchRepo.Search(&types.SchemaSearchRequest{}, 20, 0)
+	if err != nil {
+		return nil, err
+	}
+
 	return m, nil
 }
