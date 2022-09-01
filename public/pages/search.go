d4706ac15f67031a4922054e01af838b00786cf3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-09-01 08:04:40 +0200

    schema search stub

--- public/pages/search.go
+++ public/pages/search.go
@@ -2,11 +2,43 @@ package pages
 
 import (
 	"blockexchange/controller"
+	"blockexchange/types"
+	"strconv"
 )
 
 type SearchModel struct {
+	Tags    []*types.Tag
+	Schemas []*types.SchemaSearchResult
+	Query   string
+	Offset  int
+	Limit   int
 }
 
 func Search(rc *controller.RenderContext) error {
-	return rc.Render("pages/search.html", nil)
+	tags, err := rc.Repositories().TagRepo.GetAll()
+	if err != nil {
+		return err
+	}
+
+	m := &SearchModel{
+		Tags:   tags,
+		Query:  rc.Request().URL.Query().Get("q"),
+		Limit:  20,
+		Offset: 0,
+	}
+
+	page, err := strconv.ParseInt(rc.Request().URL.Query().Get("page"), 10, 64)
+	if err == nil {
+		m.Offset = int(page) * m.Limit
+	}
+
+	if m.Query != "" {
+		list, err := rc.Repositories().SchemaSearchRepo.Search(&types.SchemaSearchRequest{Keywords: &m.Query}, m.Limit, m.Offset)
+		if err != nil {
+			return err
+		}
+		m.Schemas = list
+	}
+
+	return rc.Render("pages/search.html", m)
 }
