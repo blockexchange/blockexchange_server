06196d6558f489426c4cc06e061fcb7d8733920a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-09-12 17:48:08 +0200

    tags

--- public/pages/tags.go
+++ public/pages/tags.go
@@ -2,11 +2,21 @@ package pages
 
 import (
 	"blockexchange/controller"
+	"blockexchange/types"
 )
 
 type TagsModel struct {
+	Tags []*types.Tag
 }
 
 func Tags(rc *controller.RenderContext) error {
-	return rc.Render("pages/tags.html", nil)
+	m := &TagsModel{}
+
+	tags, err := rc.Repositories().TagRepo.GetAll()
+	if err != nil {
+		return err
+	}
+	m.Tags = tags
+
+	return rc.Render("pages/tags.html", m)
 }
