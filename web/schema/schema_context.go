c2ead90eea69d40289fbdfe1e6b86c58b0b49849
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-06-11 17:44:18 +0200

    schema tags / delete

--- web/schema/schema_context.go
+++ web/schema/schema_context.go
@@ -24,6 +24,8 @@ func NewSchemaContext(tu *tmpl.TemplateUtil, repos db.Repositories, BaseURL stri
 
 func (ctx *SchemaContext) Setup(r *mux.Router) {
 	r.HandleFunc("/schema/{username}/{schemaname}", ctx.tu.OptionalSecure(ctx.Schema))
+	r.HandleFunc("/schema/{username}/{schemaname}/delete", ctx.tu.Secure(ctx.SchemaDelete, tmpl.PermissionCheck(types.JWTPermissionManagement)))
 	r.HandleFunc("/schema/{username}/{schemaname}/edit", ctx.tu.Secure(ctx.SchemaEdit, tmpl.PermissionCheck(types.JWTPermissionManagement)))
+	r.HandleFunc("/schema/{username}/{schemaname}/edit-tags", ctx.tu.Secure(ctx.SchemaTagEdit, tmpl.PermissionCheck(types.JWTPermissionManagement)))
 	r.HandleFunc("/schema/{username}", ctx.tu.OptionalSecure(ctx.UserSchema))
 }
