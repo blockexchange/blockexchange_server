9893228ad3e6cb84f6c805505df15d3549a2ccd8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-12-14 11:06:04 +0100

    user-profile component

---
+++ public/js/components/UserProfile.js
@@ -0,0 +1,49 @@
+import { search_user } from "../api/user.js";
+import format_time from "../util/format_time.js";
+
+export default {
+    props: ["name"],
+    mounted: function() {
+        if (this.name) {
+            search_user({ name: this.name })
+            .then(u => this.user = u[0]);
+        }
+    },
+	data: function() {
+		return {
+			user: null
+		};
+	},
+    methods: {
+        format_time
+    },
+	template: /*html*/`
+		<div class="row" v-if="user">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 card" style="padding: 20px;">
+				<h5>
+					User profile for
+					<small class="text-body-secondary">{{user.name}}</small>
+				</h5>
+				<ul>
+					<li>
+						<b>Username:</b> {{user.name}}
+					</li>
+					<li>
+						<b>Created:</b> {{format_time(user.created/1000)}}
+					</li>
+					<li>
+						<b>ID:</b> <span class="badge bg-success">{{user.id}}</span>
+					</li>
+					<li>
+						<b>Type:</b> <span class="badge bg-secondary">{{user.type}}</span>
+					</li>
+					<li>
+						<b>Role:</b> <span class="badge bg-secondary">{{user.role}}</span>
+					</li>
+				</ul>
+            </div>
+            <div class="col-md-4"></div>
+		</div>
+	`
+};
--- public/js/components/pages/Profile.js
+++ public/js/components/pages/Profile.js
@@ -1,9 +1,11 @@
 import Breadcrumb, { START, PROFILE } from "../Breadcrumb.js";
 import { get_claims } from "../../service/login.js";
+import UserProfile from "../UserProfile.js";
 
 export default {
 	components: {
-        "bread-crumb": Breadcrumb
+        "bread-crumb": Breadcrumb,
+		"user-profile": UserProfile
 	},
 	data: function() {
 		return {
@@ -15,30 +17,6 @@ export default {
 	},
 	template: /*html*/`
 		<bread-crumb :items="breadcrumb"/>
-		<div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4 card" style="padding: 20px;">
-				<h5>
-					User profile for
-					<small class="text-body-secondary">{{claims.username}}</small>
-				</h5>
-				<ul>
-					<li>
-						<b>Username:</b> {{claims.username}}
-					</li>
-					<li>
-						<b>ID:</b> <span class="badge bg-success">{{claims.user_id}}</span>
-					</li>
-					<li>
-						<b>Type:</b> <span class="badge bg-secondary">{{claims.type}}</span>
-					</li>
-					<li>
-						<b>Permissions:</b>
-						<span class="badge bg-secondary" v-for="permission in claims.permissions">{{permission}}</span>
-					</li>
-				</ul>
-            </div>
-            <div class="col-md-4"></div>
-		</div>
+		<user-profile :name="claims.username"/>
 	`
 };
