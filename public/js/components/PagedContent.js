936c4f82d3412d894092a5611753e6bfd270f371
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-12-14 14:33:48 +0100

    paging

--- public/js/components/PagedContent.js
+++ public/js/components/PagedContent.js
@@ -6,23 +6,43 @@ export default {
         return {
             list: [],
             busy: false,
+            per_page: 20,
             total_count: 0,
-            current_page: this.$router.currentRoute.value.query.page || 1
+            current_page: 1
         };
     },
     mounted: function() {
-        this.busy = true;
-        this.count_entries()
-        .then(c => this.total_count = c)
-        .then(() => this.fetch_entries(10, 0))
-        .then(list => this.list = list)
-        .then(() => this.busy = false);
+        this.update();
+    },
+    watch: {
+        "$route": "update"
     },
     methods: {
-
+        update: function() {
+            this.busy = true;
+            this.current_page = this.$router.currentRoute.value.query.page || 1;
+            this.count_entries()
+            .then(c => this.total_count = c)
+            .then(() => this.fetch_entries(this.per_page, (this.current_page-1)*this.per_page))
+            .then(list => this.list = list)
+            .then(() => this.busy = false);
+        }
+    },
+    computed: {
+        pages: function() {
+            return Math.ceil(this.total_count / this.per_page);
+        }
     },
     template: /*html*/`
     <slot name="header"></slot>
     <slot name="body" :list="list"></slot>
+    <div class="btn-group">
+        <router-link
+            :to="$router.currentRoute.value.path + '?page=' + i"
+            v-for="i in pages"
+            class="btn btn-xs btn-secondary">
+            {{i}}
+        </router-link>
+    </div>
     `
 };
