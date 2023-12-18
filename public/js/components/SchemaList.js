ae08206dc4de3a638a845bd33f2855eee3064443
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-12-18 11:50:10 +0100

    min-height

--- public/js/components/SchemaList.js
+++ public/js/components/SchemaList.js
@@ -14,12 +14,12 @@ export default {
     template: /*html*/`
     <div class="row">
         <div class="col-md-2" style="padding-bottom: 10px;" :key="schema.id" v-for="schema in list">
-            <div class="card" style="height: 400px;">
+            <div class="card" style="min-height: 400px;">
                 <router-link :to="schema_link(schema)">
                     <img
                         :src="preview_src(schema)"
-                        class="card-img-top img-fluid"
-                        style="background-color: #3c3737;"/>
+                        class="card-img-top"
+                        style="background-color: #3c3737; min-height: 200px;"/>
                 </router-link>
                 <div class="card-body">
                     <h5 class="card-title">
