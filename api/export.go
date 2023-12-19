82772d8dfc57f50603c496d7d27a618ff30c98a3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-12-19 07:26:36 +0100

    cleanup

--- api/export.go
+++ api/export.go
@@ -7,9 +7,40 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+func (api *Api) incrementDownloadstats(schema_id int64, r *http.Request) error {
+	origin := r.Header.Get("X-Forwarded-For")
+	origin_parts := strings.Split(origin, ",")
+	if len(origin_parts) >= 1 {
+		ip := strings.TrimSpace(origin_parts[0])
+		cache_key := fmt.Sprintf("download_marker/%d/%s", schema_id, ip)
+
+		m, err := api.Cache.Get(cache_key)
+		if err != nil {
+			return err
+		}
+		if m != nil {
+			//already incremented stat for this ip, skip
+			return nil
+		}
+		// mark as incremented
+		err = api.Cache.Set(cache_key, []byte{0x00}, time.Hour*24)
+		if err != nil {
+			return err
+		}
+	}
+
+	err := api.SchemaRepo.IncrementDownloads(schema_id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func (api *Api) ExportWorldeditSchema(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -36,7 +67,7 @@ func (api *Api) ExportWorldeditSchema(w http.ResponseWriter, r *http.Request) {
 		SendError(w, 500, err.Error())
 	}
 
-	err = api.SchemaRepo.IncrementDownloads(int64(id))
+	err = api.incrementDownloadstats(int64(id), r)
 	if err != nil {
 		SendError(w, 500, err.Error())
 		return
@@ -83,7 +114,7 @@ func (api *Api) ExportBXSchema(w http.ResponseWriter, r *http.Request) {
 		SendError(w, 500, err.Error())
 	}
 
-	err = api.SchemaRepo.IncrementDownloads(int64(id))
+	err = api.incrementDownloadstats(int64(id), r)
 	if err != nil {
 		SendError(w, 500, err.Error())
 		return
