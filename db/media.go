d8306a15a8b4942556e5eb88259da87f0456800d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-28 13:49:29 +0100

    db api

--- db/media.go
+++ db/media.go
@@ -7,11 +7,13 @@ import (
 	"github.com/vingarcia/ksql"
 )
 
-var modTable = ksql.NewTable("mod", "name")
-
 type MediaRepository struct {
 	kdb ksql.Provider
 }
+
+// mod
+
+var modTable = ksql.NewTable("mod", "name")
 
 func (r *MediaRepository) CreateMod(m *types.Mod) error {
 	return r.kdb.Insert(context.Background(), modTable, m)
@@ -39,4 +41,67 @@ func (r *MediaRepository) GetMods() ([]*types.Mod, error) {
 
 func (r *MediaRepository) RemoveMod(name string) error {
 	return r.kdb.Delete(context.Background(), modTable, name)
+}
+
+// nodedef
+
+var nodedefTable = ksql.NewTable("nodedefinition", "name")
+
+func (r *MediaRepository) CreateNodedefinition(nd *types.Nodedefinition) error {
+	return r.kdb.Insert(context.Background(), nodedefTable, nd)
+}
+
+func (r *MediaRepository) UpdateNodedefinition(nd *types.Nodedefinition) error {
+	return r.kdb.Patch(context.Background(), nodedefTable, nd)
+}
+
+func (r *MediaRepository) GetNodedefinitionByName(name string) (*types.Nodedefinition, error) {
+	nd := &types.Nodedefinition{}
+	err := r.kdb.QueryOne(context.Background(), nd, "from nodedefinition where name = $1", name)
+	if err == ksql.ErrRecordNotFound {
+		return nil, nil
+	} else {
+		return nd, err
+	}
+}
+func (r *MediaRepository) GetNodedefinitions() ([]*types.Nodedefinition, error) {
+	list := []*types.Nodedefinition{}
+	err := r.kdb.Query(context.Background(), &list, "from nodedefinition")
+	return list, err
+}
+
+func (r *MediaRepository) RemoveNodedefinition(name string) error {
+	return r.kdb.Delete(context.Background(), nodedefTable, name)
+}
+
+// mediafile
+
+var mediafileTable = ksql.NewTable("mediafile", "name")
+
+func (r *MediaRepository) CreateMediafile(mf *types.Mediafile) error {
+	return r.kdb.Insert(context.Background(), mediafileTable, mf)
+}
+
+func (r *MediaRepository) UpdateMediafile(mf *types.Mediafile) error {
+	return r.kdb.Patch(context.Background(), mediafileTable, mf)
+}
+
+func (r *MediaRepository) GetMediafileByName(name string) (*types.Mediafile, error) {
+	mf := &types.Mediafile{}
+	err := r.kdb.QueryOne(context.Background(), mf, "from mediafile where name = $1", name)
+	if err == ksql.ErrRecordNotFound {
+		return nil, nil
+	} else {
+		return mf, err
+	}
+}
+
+func (r *MediaRepository) GetMediafiles() ([]*types.Mediafile, error) {
+	list := []*types.Mediafile{}
+	err := r.kdb.Query(context.Background(), &list, "from mediafile")
+	return list, err
+}
+
+func (r *MediaRepository) RemoveMediafile(name string) error {
+	return r.kdb.Delete(context.Background(), mediafileTable, name)
 }
--- db/media_test.go
+++ db/media_test.go
@@ -48,3 +48,67 @@ func TestModEntity(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Nil(t, mod2)
 }
+
+func TestModMedia(t *testing.T) {
+	repos := CreateTestDatabase(t)
+	mr := repos.MediaRepo
+
+	mod := &types.Mod{
+		Name:         "mymod",
+		Source:       "https://...",
+		CodeLicense:  "MIT",
+		MediaLicense: "CC-BY-3.0",
+	}
+
+	// prepare
+	mr.RemoveMod("mymod")
+	assert.NoError(t, mr.CreateMod(mod))
+
+	// create nodedef
+	nd := &types.Nodedefinition{
+		Name:       "mymod:xy",
+		ModName:    "mymod",
+		Definition: "{}",
+	}
+	assert.NoError(t, mr.CreateNodedefinition(nd))
+
+	// get nodedef
+	nd2, err := mr.GetNodedefinitionByName("mymod:xy")
+	assert.NoError(t, err)
+	assert.NotNil(t, nd2)
+	assert.EqualValues(t, nd, nd2)
+
+	// get all nodedefs
+	nodedefs, err := mr.GetNodedefinitions()
+	assert.NoError(t, err)
+	assert.NotNil(t, nodedefs)
+	assert.True(t, len(nodedefs) >= 1)
+
+	// update
+	nd.Definition = `{"x":1}`
+	assert.NoError(t, mr.UpdateNodedefinition(nd))
+
+	// remove
+	assert.NoError(t, mr.RemoveNodedefinition("mymod:xy"))
+
+	// create media
+	mf := &types.Mediafile{
+		Name:    "default_stone.png",
+		ModName: "mymod",
+		Data:    []byte{0x00},
+	}
+	assert.NoError(t, mr.CreateMediafile(mf))
+
+	// get media
+	mf2, err := mr.GetMediafileByName("default_stone.png")
+	assert.NoError(t, err)
+	assert.NotNil(t, mf2)
+	assert.EqualValues(t, mf, mf2)
+
+	// remove media
+	assert.NoError(t, mr.RemoveMediafile("default_stone.png"))
+
+	// remove all
+	assert.NoError(t, mr.RemoveMod("mymod"))
+
+}
