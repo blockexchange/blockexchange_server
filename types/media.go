440fd08e17a1f776ef583586f4f98c303493c85f
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-29 10:47:59 +0100

    mediafile unmarshaller

--- types/media.go
+++ types/media.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"encoding/base64"
+	"encoding/json"
+)
+
 type Mod struct {
 	Name         string `json:"name" ksql:"name"`
 	Source       string `json:"source" ksql:"source"`
@@ -16,4 +21,21 @@ type Mediafile struct {
 	Name    string `json:"name" ksql:"name"`
 	ModName string `json:"mod_name" ksql:"mod_name"`
 	Data    []byte `json:"data" ksql:"data"`
+}
+
+func (mf *Mediafile) UnmarshalJSON(data []byte) error {
+	m := make(map[string]any)
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return err
+	}
+
+	mf.ModName = getString(m["mod_name"])
+	mf.Name = getString(m["name"])
+	mf.Data, err = base64.RawStdEncoding.DecodeString(getString(m["data"]))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
