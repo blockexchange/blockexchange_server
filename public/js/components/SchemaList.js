2260f4fe524b92bdf005c7faa0a19e8619b218df
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-06-25 17:30:40 +0200

    preview fixes

--- public/js/components/SchemaList.js
+++ public/js/components/SchemaList.js
@@ -27,7 +27,7 @@ export default {
                 <router-link :to="schema_link(entry)">
                     <img
                         :src="preview_src(entry.schema)"
-                        style="background-color: #3c3737; min-height: 200px; display: block; margin-left: auto; margin-right: auto;"/>
+                        style="background-color: #303030; min-height: 200px; display: block; margin-left: auto; margin-right: auto; max-width: 100%;"/>
                 </router-link>
                 <div class="card-body">
                     <h5 class="card-title">
