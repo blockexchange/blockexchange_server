e395b3a0368e7cc61289420d3f395ca0ca03ae80
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-11-17 10:40:44 +0100

    postgres lock overhaul

--- jobs/jobs.go
+++ jobs/jobs.go
@@ -5,16 +5,10 @@ import (
 	"blockexchange/core"
 	"blockexchange/db"
 	"blockexchange/types"
-	"fmt"
 	"time"
 
+	"cirello.io/pglock"
 	"github.com/sirupsen/logrus"
-)
-
-const (
-	LOCKID_CLEANUP      = 100
-	LOCKID_STATS_UPDATE = 101
-	LOCKID_SCREENSHOT   = 102
 )
 
 func Start(repos *db.Repositories, api *api.Api) {
@@ -23,75 +17,95 @@ func Start(repos *db.Repositories, api *api.Api) {
 
 	if cfg.ExecuteJobs {
 		// start jobs
-		go cleanupSchemas(repos.SchemaRepo, repos.Lock)
-		go updateScreenshots(c, api.SchemaSearchRepo, repos.Lock)
+		go cleanupSchemas(repos.SchemaRepo, repos.PGLock)
+		go updateScreenshots(c, api.SchemaSearchRepo, repos.PGLock)
 	}
 	// per-node stats job
-	go updateStats(api, repos.Lock)
+	go updateStats(api, repos.PGLock)
 
 }
 
-func updateStats(api *api.Api, lock *db.DBLock) {
+func updateStats(api *api.Api, pgl *pglock.Client) {
 	for {
-		err := lock.RunLocked(LOCKID_STATS_UPDATE, time.Minute, api.UpdateStats)
+		lock, err := pgl.Acquire("update-stats")
 		if err != nil {
-			logrus.WithError(err).Error("stats update")
+			logrus.WithError(err).Error("update stats lock")
+			time.Sleep(time.Second * 10)
+			continue
 		}
+
+		err = api.UpdateStats()
+		if err != nil {
+			logrus.WithError(err).Error("update stats")
+		}
+
+		lock.Close()
 		time.Sleep(30 * time.Minute)
 	}
 }
 
-func cleanupSchemas(schemarepo *db.SchemaRepository, lock *db.DBLock) {
+func cleanupSchemas(schemarepo *db.SchemaRepository, pgl *pglock.Client) {
 	for {
-		err := lock.RunLocked(LOCKID_CLEANUP, time.Minute, func() error {
-			logrus.Trace("Removing old and incomplete schemas")
-			now := time.Now().Unix() * 1000
-			return schemarepo.DeleteOldIncompleteSchema(now - (3600 * 1000 * 24))
-		})
+		lock, err := pgl.Acquire("schema-cleanup")
 		if err != nil {
-			logrus.WithError(err).Error("cleanupSchemas")
+			logrus.WithError(err).Error("schema cleanup lock")
+			time.Sleep(time.Second * 10)
+			continue
 		}
+
+		logrus.Trace("Removing old and incomplete schemas")
+		now := time.Now().Unix() * 1000
+		err = schemarepo.DeleteOldIncompleteSchema(now - (3600 * 1000 * 24))
+		if err != nil {
+			logrus.WithError(err).Error("schema cleanup")
+		}
+
+		lock.Close()
 		time.Sleep(5 * time.Minute)
 	}
 }
 
-func updateScreenshots(c *core.Core, sr *db.SchemaSearchRepository, lock *db.DBLock) {
+func updateScreenshots(c *core.Core, sr *db.SchemaSearchRepository, pgl *pglock.Client) {
 	from := time.Now().Add(-10*time.Minute).Unix() * 1000
 
 	for {
-		logrus.Trace("updating schema previews")
-		err := lock.RunLocked(LOCKID_SCREENSHOT, time.Minute, func() error {
-			complete := true
-			list, err := sr.Search(&types.SchemaSearchRequest{
-				FromMtime: &from,
-				Complete:  &complete,
-			})
-			if err != nil {
-				return fmt.Errorf("schema search error: %v", err)
-			}
-
-			for _, r := range list {
-				logrus.WithFields(logrus.Fields{
-					"uid":   r.Schema.UID,
-					"mtime": r.Schema.Mtime,
-				}).Debug("Updating schema screenshot")
-				_, err = c.UpdatePreview(r.Schema)
-				if err != nil {
-					return fmt.Errorf("schema preview update error: '%s', %v", r.Schema.UID, err)
-				}
-
-				// shift mtime window to max mtime from result
-				if r.Schema.Mtime > from {
-					from = r.Schema.Mtime
-				}
-			}
-			return nil
-		})
+		lock, err := pgl.Acquire("update-screenshots")
 		if err != nil {
-			logrus.WithError(err).Errorf("schema screenshot update: %d", from)
+			logrus.WithError(err).Error("screenshot update lock")
+			time.Sleep(time.Second * 10)
 			continue
 		}
 
+		logrus.Trace("updating schema previews")
+		complete := true
+		list, err := sr.Search(&types.SchemaSearchRequest{
+			FromMtime: &from,
+			Complete:  &complete,
+		})
+		if err != nil {
+			logrus.WithError(err).Error("schema search")
+			time.Sleep(time.Second * 10)
+			continue
+		}
+
+		for _, r := range list {
+			logrus.WithFields(logrus.Fields{
+				"uid":   r.Schema.UID,
+				"mtime": r.Schema.Mtime,
+			}).Debug("Updating schema screenshot")
+			_, err = c.UpdatePreview(r.Schema)
+			if err != nil {
+				logrus.Errorf("schema preview update error: '%s', %v", r.Schema.UID, err)
+				continue
+			}
+
+			// shift mtime window to max mtime from result
+			if r.Schema.Mtime > from {
+				from = r.Schema.Mtime
+			}
+		}
+
+		lock.Close()
 		time.Sleep(5 * time.Minute)
 	}
 }
